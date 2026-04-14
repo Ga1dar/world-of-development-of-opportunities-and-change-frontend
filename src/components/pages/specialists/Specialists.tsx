c17a6578b0a439a0a28fdcc79e1efc6809bb437e
Kyrylo Golikov <pipecc1985@gmail.com>
2026-04-14 22:05:13 +0200

    translator

--- src/components/pages/specialists/Specialists.tsx
+++ src/components/pages/specialists/Specialists.tsx
@@ -16,13 +16,17 @@ export function Specialists({ limit }: Props) {
   }, [limit]);
 
   return (
-    <section className="specialistsSection">
+    <section className="flex h-full w-full flex-col gap-y-6 md:flex-row md:gap-x-6 md:justify-center">
       {data.map((item) => (
-        <article key={item.id} className="specialistCard">
-          <img className="specialistPhoto" src={item.photo} alt={item.name} />
+        <article key={item.id} className="w-full md:flex md:flex-col md:items-center md:gap-y-4 md:px-4 md:pb-4 md:pt-0">
+          <img
+            className="inline-block md:w-64 md:h-64 md:rounded-full"
+            src={item.photo}
+            alt={item.name}
+          />
 
-          <h3 className="specialistName">{item.name}</h3>
-          <p className="specialistRole">{item.role}</p>
+          <h3 className="font-montserrat font-medium text-primary-foreground md:h-14 md:text-2xl">{item.name}</h3>
+          <p className="font-montserrat font-medium text-primary-foreground md:h-6 md:text-lg">{item.role}</p>
         </article>
       ))}
     </section>
